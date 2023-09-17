ead0cf0fb5b946ae5de26b4f8db1ad21256f6a4d
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2023-09-17 17:54:33 +0530

    Initial

--- src/Reverse_Linked_list.java
+++ src/Reverse_Linked_list.java
@@ -14,9 +14,13 @@ public class Reverse_Linked_list {
       Node a = new Node(1);
         Node b = new Node(2);
         Node c = new Node(3);
+        Node d = new Node(4);
+        Node e = new Node(5);
 
         a.next = b;
         b.next = c;
+        c.next = d;
+        d.next = e;
 
         Node temp = a;
         while (temp!=null){
